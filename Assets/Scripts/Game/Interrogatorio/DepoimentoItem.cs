a1b6918587b40b6b735f1bc54bf0f3e91ca5edd2
Chris <chrisdbhr@gmail.com>
2023-06-18 22:44:33 -0300

    depoimento wip

--- Assets/Scripts/Game/Interrogatorio/DepoimentoItem.cs
+++ Assets/Scripts/Game/Interrogatorio/DepoimentoItem.cs
@@ -1,7 +1,18 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace Game.Interrogatorio {
 	public class DepoimentoItem : MonoBehaviour {
+
+		[SerializeField] private TextMeshProUGUI _text;
+		private DepoimentosManager.Depoimento _depoimento;
+		
+		
+		public void Initialize(DepoimentosManager.Depoimento depoimento) {
+			this._depoimento = depoimento;
+			this._text.text = depoimento.Pessoa;
+			this.gameObject.SetActive(true);
+		}
 		
 	}
 }
