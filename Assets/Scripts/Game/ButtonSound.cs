f019dc2f8d2e9c47605d569ec6635e7f3910ea28
Chris <chrisdbhr@gmail.com>
2023-06-21 21:48:43 -0300

    button sound

--- Assets/Scripts/Game/ButtonSound.cs
+++ Assets/Scripts/Game/ButtonSound.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CDK;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,20 +10,18 @@ namespace Game {
 	public class ButtonSound : MonoBehaviour {
 
 		[SerializeField] private ButtonSoundData _soundData;
-		private AudioSource _audioSource;
 		private Button _button;
 
 
+		
+		
 		private void Awake() {
-			this._audioSource = this.gameObject.AddComponent<AudioSource>();
 			_button = this.GetComponent<Button>();
 		}
 
 		private void OnEnable() {
 			_button.OnClickAsObservable().TakeUntilDisable(this).Subscribe(_ => {
-				this._audioSource.clip = _soundData.ButtonSound;
-				this._audioSource.volume = _soundData.ButtonSoundVolume;
-				this._audioSource.Play();
+				CSoundManager.get.PlayOneShot(_soundData.ButtonSound, _soundData.ButtonSoundVolume);
 			});
 		}
 	}
