d40a9bf0804963140c8ad834aa08b8343315da37
Chris <chrisdbhr@gmail.com>
2023-06-21 21:32:22 -0300

    button sound

---
+++ Assets/Scripts/Game/ButtonSound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game {
+	
+	[RequireComponent(typeof(Button))]
+	public class ButtonSound : MonoBehaviour {
+
+		[SerializeField] private ButtonSoundData _soundData;
+		private AudioSource _audioSource;
+		private Button _button;
+
+
+		private void Awake() {
+			this._audioSource = this.gameObject.AddComponent<AudioSource>();
+			_button = this.GetComponent<Button>();
+		}
+
+		private void OnEnable() {
+			_button.OnClickAsObservable().TakeUntilDisable(this).Subscribe(_ => {
+				this._audioSource.clip = _soundData.ButtonSound;
+				this._audioSource.volume = _soundData.ButtonSoundVolume;
+				this._audioSource.Play();
+			});
+		}
+	}
+}
---
+++ Assets/Scripts/Game/ButtonSoundData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game {
+	public class ButtonSoundData : ScriptableObject {
+
+		public AudioClip ButtonSound;
+		[Range(0f,1f)]public float ButtonSoundVolume = 1f;
+
+
+	}
+}
