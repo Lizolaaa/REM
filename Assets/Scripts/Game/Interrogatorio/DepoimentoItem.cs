50bb66cdafa8cf3d9ed6805e0debbdbed7a7d00a
Chris <chrisdbhr@gmail.com>
2023-06-21 21:00:31 -0300

    depoimento script

--- Assets/Scripts/Game/Interrogatorio/DepoimentoItem.cs
+++ Assets/Scripts/Game/Interrogatorio/DepoimentoItem.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 namespace Game.Interrogatorio {
 	public class DepoimentoItem : MonoBehaviour {
 
-		private DepoimentosManager.Depoimento _depoimento;
-		[SerializeField] private TextMeshProUGUI _nameText;
-		[SerializeField] private TextMeshProUGUI _text;
+		[SerializeField] private TextMeshProUGUI _textName;
+		[SerializeField] private TextMeshProUGUI _textDepoimentos;
 
-		
+		private DepoimentosManager.Depoimento _depoimento;
+
 		
 		
 		public void Initialize(DepoimentosManager.Depoimento depoimento) {
 			this._depoimento = depoimento;
-			this._nameText.text = depoimento.Pessoa;
-			this._text.text = string.Join(Environment.NewLine, depoimento.Depoimentos);
+			this._textName.text = depoimento.Pessoa;
+			this._textDepoimentos.text = string.Join(Environment.NewLine, depoimento.Depoimentos);
 			this.gameObject.SetActive(true);
 		}
 		
